5100516a33c211a0723f377677c7aef1108f25a5
noobertx <noobertx@gmail.com>
2020-01-20 23:10:24 +0800

    Initial Commit

---
+++ divi-custom-modules.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Plugin Name: Noobertx 8 Divi Modules
+ * Description: 8 Divi Modules  : Advance Backgrounds, Animated Content, Interactive Banner, Animated Text, Highlight Box, Cube Box, Flipbox , Info Banner
+ */
